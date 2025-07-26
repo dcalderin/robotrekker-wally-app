a48c543230fcb4455abe97f48b518bc464f68f65
dcalderin <87214811+dcalderin@users.noreply.github.com>
2025-07-26 11:44:00 +0200

    Update auth config for production deployment

--- config/authConfig.js
+++ config/authConfig.js
@@ -1,8 +1,9 @@
+// config/authConfig.js
 export const msalConfig = {
   auth: {
-    clientId: '04e2e4c0-153d-4d41-870e-41aab6af485b', // Your app client ID
-    authority: 'https://login.microsoftonline.com/47ad30b9-4668-4bb3-a485-0a1c23a66679', // Your tenant ID
-    redirectUri: 'http://localhost:3000'
+    clientId: '04e2e4c0-153d-4d41-870e-41aab6af485b',
+    authority: 'https://login.microsoftonline.com/47ad30b9-4668-4bb3-a485-0a1c23a66679',
+    redirectUri: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000'
   },
   cache: {
     cacheLocation: 'sessionStorage',
@@ -11,5 +12,5 @@ export const msalConfig = {
 };
 
 export const loginRequest = {
-  scopes: ['openid', 'profile', 'User.Read'],
+  scopes: ['openid', 'profile', 'email']
 };
