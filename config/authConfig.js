fdd011607d64a8c3ed4939918e8f11c2aabe9886
dcalderin <87214811+dcalderin@users.noreply.github.com>
2025-07-26 12:50:04 +0200

    Fix auth config - use tenant ID for External ID

--- config/authConfig.js
+++ config/authConfig.js
@@ -2,14 +2,19 @@
 export const msalConfig = {
   auth: {
     clientId: '04e2e4c0-153d-4d41-870e-41aab6af485b',
-    // For External ID tenants, use the specific tenant subdomain
-    authority: 'https://login.microsoftonline.com/robotrekerai.onmicrosoft.com',
-    redirectUri: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000',
-    // Add these for External ID
-    knownAuthorities: ['robotrekerai.onmicrosoft.com'],
-    cloudDiscoveryMetadata: '',
-    authorityMetadata: ''
+    // Use the External ID tenant where your app is registered
+    authority: 'https://login.microsoftonline.com/47ad30b9-4668-4bb3-a485-0a1c23a66679',
+    redirectUri: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000'
   },
+  cache: {
+    cacheLocation: 'sessionStorage',
+    storeAuthStateInCookie: false,
+  }
+};
+
+export const loginRequest = {
+  scopes: ['openid', 'profile', 'email']
+};
   cache: {
     cacheLocation: 'sessionStorage',
     storeAuthStateInCookie: false,
