a7bf222a1765e14bcc2a5972a470a437eb60bfa2
dcalderin <87214811+dcalderin@users.noreply.github.com>
2025-07-27 19:07:14 +0200

    Fix External ID CIAM authority URL call back and redirect

--- config/authConfig.js
+++ config/authConfig.js
@@ -2,7 +2,7 @@
 export const msalConfig = {
   auth: {
     clientId: '04e2e4c0-153d-4d41-870e-41aab6af485b',
-    authority: 'https://robotrekerai.ciamlogin.com/47ad30b9-4668-4bb3-a485-0a1c23a66679',
+    authority: 'https://robotrekerai.ciamlogin.com/47ad30b9-4668-4bb3-a485-0a1c23a66679/v2',
     redirectUri: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:3000/authentication/login-callback',
     knownAuthorities: ['robotrekerai.ciamlogin.com']
   },
